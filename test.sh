ab2052c1d31c87f7a4c168c253717dc2b84d8736
aerth <aerth@riseup.net>
2018-10-15 13:27:33 -0700

    tests: output formatting

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+echo "Namecoin PKCS#11 Module Test"
 so=./libnamecoin.so
-pkcs11-dump info $so
+
+printf '\n\nModule information\n\n'
+pkcs11-dump info $so 2>>/dev/null
+
+printf '\n\nTrying to make a key with module\n\n'
 ssh-keygen -D $so
 #https://docs.fedoraproject.org/en-US/Fedora/26/html/System_Administrators_Guide/sec-SSH_Certificate_PKCS_11_Token.html
