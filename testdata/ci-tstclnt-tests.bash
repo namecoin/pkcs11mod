0b9462aa08779857d08a69c217a3886860b956dd
Jeremy Rand <jeremyrand@airmail.cc>
2021-06-20 16:04:28 +0000

    Add p11proxy tstclnt tests

--- testdata/ci-tstclnt-tests.bash
+++ testdata/ci-tstclnt-tests.bash
@@ -35,3 +35,14 @@ testdata/assert-proxy-log.bash present
 
 testdata/try-tstclnt-connect.bash untrusted-root.badssl.com fail "SEC_ERROR_UNTRUSTED_ISSUER"
 testdata/assert-proxy-log.bash present
+
+echo "===== p11-kit-trust CKBI via p11proxy ====="
+
+export P11PROXY_CKBI_TARGET=/usr/lib64/pkcs11/p11-kit-trust.orig.so
+cp libp11proxy.so /usr/lib64/pkcs11/p11-kit-trust.so
+
+testdata/try-tstclnt-connect.bash www.namecoin.org success ""
+testdata/assert-proxy-log.bash present
+
+testdata/try-tstclnt-connect.bash untrusted-root.badssl.com fail "SEC_ERROR_UNTRUSTED_ISSUER"
+testdata/assert-proxy-log.bash present
