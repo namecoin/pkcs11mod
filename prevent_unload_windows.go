d0cdc19bdf77415145fef50493310b2297e50757
Jeremy Rand <jeremyrand@danwin1210.de>
2022-04-25 11:03:17 +0000

    pkcs11mod: Avoid hardcoding module name

--- prevent_unload_windows.go
+++ prevent_unload_windows.go
@@ -4,6 +4,8 @@ package pkcs11mod
 
 import (
 	"log"
+	"reflect"
+	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
@@ -15,19 +17,13 @@ import (
 // https://blogs.msmvps.com/vandooren/2006/10/09/preventing-a-dll-from-being-unloaded-by-the-app-that-uses-it/
 // https://stackoverflow.com/a/14436845
 // https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulehandleexa?redirectedfrom=MSDN#parameters
+// https://github.com/pipelined/vst2/blob/bc659a1443b585c376cc25a6d13614488c9fa4ee/plugin_export_windows.go#L11-L34
 func preventUnload() {
-	// TODO: don't hardcode this module name
-
-	moduleNameGo := "nssckbi"
-
-	moduleNameWin, err := windows.UTF16PtrFromString(moduleNameGo)
-	if err != nil {
-		log.Printf("pkcs11mod: Error converting module name: %s", err)
-		return
-	}
+	// See https://pkg.go.dev/unsafe#Pointer (reflect.Value.Pointer)
+	moduleAddressWin := (*uint16)(unsafe.Pointer(reflect.ValueOf(preventUnload).Pointer()))
 
 	var module windows.Handle
-	err = windows.GetModuleHandleEx(windows.GET_MODULE_HANDLE_EX_FLAG_PIN, moduleNameWin, &module)
+	err := windows.GetModuleHandleEx(windows.GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS|windows.GET_MODULE_HANDLE_EX_FLAG_PIN, moduleAddressWin, &module)
 	if err != nil {
 		log.Printf("pkcs11mod: Error pinning module: %s", err)
 		return
