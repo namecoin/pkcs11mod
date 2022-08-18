42c4189b67c5858461ea2006be7c1e55c98090e3
Jeremy Rand <jeremyrand@danwin1210.de>
2022-08-18 01:31:29 +0000

    Remove redundant types in function declarations
    Reported by gofumpt -extra.

--- pkcs11mod.go
+++ pkcs11mod.go
@@ -464,7 +464,7 @@ func goGetOperationState(sessionHandle C.CK_SESSION_HANDLE, pOperationState C.CK
 }
 
 //export goSetOperationState
-func goSetOperationState(sessionHandle C.CK_SESSION_HANDLE, pOperationState C.CK_BYTE_PTR, ulOperationStateLen C.CK_LONG, hEncryptionKey C.CK_OBJECT_HANDLE, hAuthenticationKey C.CK_OBJECT_HANDLE) C.CK_RV {
+func goSetOperationState(sessionHandle C.CK_SESSION_HANDLE, pOperationState C.CK_BYTE_PTR, ulOperationStateLen C.CK_LONG, hEncryptionKey, hAuthenticationKey C.CK_OBJECT_HANDLE) C.CK_RV {
 	if pOperationState == nil {
 		return C.CKR_ARGUMENTS_BAD
 	}
@@ -1416,7 +1416,7 @@ func goGenerateKey(sessionHandle C.CK_SESSION_HANDLE, pMechanism C.CK_MECHANISM_
 }
 
 //export goGenerateKeyPair
-func goGenerateKeyPair(sessionHandle C.CK_SESSION_HANDLE, pMechanism C.CK_MECHANISM_PTR, pPublicKeyTemplate C.CK_ATTRIBUTE_PTR, ulPublicKeyAttributeCount C.CK_ULONG, pPrivateKeyTemplate C.CK_ATTRIBUTE_PTR, ulPrivateKeyAttributeCount C.CK_ULONG, phPublicKey C.CK_OBJECT_HANDLE_PTR, phPrivateKey C.CK_OBJECT_HANDLE_PTR) C.CK_RV {
+func goGenerateKeyPair(sessionHandle C.CK_SESSION_HANDLE, pMechanism C.CK_MECHANISM_PTR, pPublicKeyTemplate C.CK_ATTRIBUTE_PTR, ulPublicKeyAttributeCount C.CK_ULONG, pPrivateKeyTemplate C.CK_ATTRIBUTE_PTR, ulPrivateKeyAttributeCount C.CK_ULONG, phPublicKey, phPrivateKey C.CK_OBJECT_HANDLE_PTR) C.CK_RV {
 	if pMechanism == nil || pPublicKeyTemplate == nil || pPrivateKeyTemplate == nil {
 		return C.CKR_ARGUMENTS_BAD
 	}
@@ -1437,7 +1437,7 @@ func goGenerateKeyPair(sessionHandle C.CK_SESSION_HANDLE, pMechanism C.CK_MECHAN
 }
 
 //export goWrapKey
-func goWrapKey(sessionHandle C.CK_SESSION_HANDLE, pMechanism C.CK_MECHANISM_PTR, hWrappingKey C.CK_OBJECT_HANDLE, hKey C.CK_OBJECT_HANDLE, pWrappedKey C.CK_BYTE_PTR, pulWrappedKeyLen C.CK_ULONG_PTR) C.CK_RV {
+func goWrapKey(sessionHandle C.CK_SESSION_HANDLE, pMechanism C.CK_MECHANISM_PTR, hWrappingKey, hKey C.CK_OBJECT_HANDLE, pWrappedKey C.CK_BYTE_PTR, pulWrappedKeyLen C.CK_ULONG_PTR) C.CK_RV {
 	if pMechanism == nil || pWrappedKey == nil || pulWrappedKeyLen == nil {
 		return C.CKR_ARGUMENTS_BAD
 	}
