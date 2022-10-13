4edc8c7f0893c3b5ee14d56d189eaa3303624db1
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-10-13 09:30:42 +0300

    Update p11mod.go
    Description for the objects slice

--- p11mod/p11mod.go
+++ p11mod/p11mod.go
@@ -56,8 +56,9 @@ func init() {
 
 type llSession struct {
 	session         p11.Session
-	slotID          uint
-	objects         []p11.Object //Used to keep track of mapping between object handles and objects, because object handles don't exist in the p11 API
+	slotID          
+	//Used to keep track of mapping between object handles and objects, because object handles don't exist in the p11 API
+	objects         []p11.Object
 	objectsPending  []p11.Object
 	signMechanism   *pkcs11.Mechanism
 	signKeyIndex    int
