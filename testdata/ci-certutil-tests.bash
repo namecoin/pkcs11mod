58d02d607b0ac142b3879c75fa25f4c2c4b097d2
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-11-09 15:21:16 +0300

    softtoken tests

--- testdata/ci-certutil-tests.bash
+++ testdata/ci-certutil-tests.bash
@@ -8,7 +8,7 @@ export P11PROXY_CKBI_TARGET=$PKCS11PROXY_CKBI_TARGET
 
 echo "===== init DB ====="
 
-ln -s -T "$CI_MAIN_MODULE" ./libnssckbi.so
+ln -s -T "$CI_MAIN_MODULE" ./libsoftokn3.so
 certutil -N -d . --empty-password
 
 echo "===== list all (default) ====="
@@ -21,7 +21,7 @@ testdata/assert-proxy-log.bash missing
 echo "===== list all (via pkcs11proxy) ====="
 
 mv "$CI_MAIN_MODULE" "$CI_BAK_MODULE"
-cp libpkcs11proxy.so "$CI_MAIN_MODULE"
+cp libsoftokn3proxy.so "$CI_MAIN_MODULE"
 
 certutil -L -d . -h all | tee list-all-pkcs11proxy.txt || true
 grep -q ",C," list-all-pkcs11proxy.txt
@@ -33,7 +33,7 @@ testdata/assert-proxy-log.bash present
 
 echo "===== list all (via p11proxy) ====="
 
-cp libp11proxy.so "$CI_MAIN_MODULE"
+cp libsoftokn3proxy.so "$CI_MAIN_MODULE"
 
 certutil -L -d . -h all | tee list-all-p11proxy.txt || true
 grep -q ",C," list-all-p11proxy.txt
